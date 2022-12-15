9838813f4ef62012326bf4535943f313d71dab72
Lucimara Souza <lucimarasouzah@gmail.com>
2022-12-14 22:22:47 -0300

    Instalação Angular e Bootstrap

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('http://localhost:5041/api/eventos').subscribe(
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
+    );
+  }
+}
